c4989084872337cee849bdf4148c8e55168d2e3d
Sibongile <sibongile.nhlema@gmail.com>
2024-03-22 21:48:57 +0200

    completed task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' Script that starts a flask web application on four routes
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_hbhb(text):
+    ''' fuxntion that handles variables '''
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/<text>', strict_slash=False)
+def py_hbnb(text="is cool"):
+    ''' function that handles variables '''
+    text = text.replace()'_', ' ')
+    return f'Python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
